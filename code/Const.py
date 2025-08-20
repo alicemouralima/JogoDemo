7421aa3b0d338766a83a0fed6f506e499f5f32c6
alice <aliceml843@gmail.com>
2025-08-19 22:51:50 -0300

    Creating menu interations Creating ABC class And more...

--- code/Const.py
+++ code/Const.py
@@ -2,6 +2,16 @@
 COLOR_BROWN = (139, 0, 0)
 COLOR_WHITE = (255, 255, 255)
 COLOR_YELLOW = (255, 253, 85)
+
+# E
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4
+}
+
 # M
 MENU_OPTION = ('NEW GAME 1P' ,
                'NEW GAME 2P - COOPERATIVE' ,
